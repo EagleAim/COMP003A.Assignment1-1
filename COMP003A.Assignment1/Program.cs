d865315f19be03c0032acdd57561335f315387d8
Emma <Emma@EMMAPC>
2024-01-21 23:29:16 -0800

    Add project files.

---
+++ COMP003A.Assignment1/Program.cs
@@ -0,0 +1,116 @@
+﻿/* 
+Author: Logan King
+Purpouse: The purpouse of this assignment is to introduce us to C# and visual studio.
+*/
+
+
+namespace COMP003A.Assignment1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("100 Things About Me");
+            Console.WriteLine("1.I like steak");
+            Console.WriteLine("2.I like corn");
+            Console.WriteLine("3.I like shrimp");
+            Console.WriteLine("4.I like pancakes");
+            Console.WriteLine("5.I like apples");
+            Console.WriteLine("6.I like games");
+            Console.WriteLine("7.I like apex");
+            Console.WriteLine("8.I like MW3");
+            Console.WriteLine("9.I like BL2");
+            Console.WriteLine("10.I like BL1");
+            Console.WriteLine("11.I like BL3");
+            Console.WriteLine("12.I like BLPS");
+            Console.WriteLine("13.I like Tiny Tinas Wonderlands");
+            Console.WriteLine("14.I like Ready or Not");
+            Console.WriteLine("15.I like coding");
+            Console.WriteLine("16.I like building pcs");
+            Console.WriteLine("17.I like messing with pcs");
+            Console.WriteLine("18.I like playing with freinds");
+            Console.WriteLine("19.I like my S.O");
+            Console.WriteLine("20.I like cooking");
+            Console.WriteLine("21.I like helping people");
+            Console.WriteLine("22.I like playing Lethal Company");
+            Console.WriteLine("23.I like The finals");
+            Console.WriteLine("24.I like Vintage Story");
+            Console.WriteLine("25.I like Rainbow 6 siege");
+            Console.WriteLine("26.I like Team fortress 2");
+            Console.WriteLine("27.I like Titian fall 2");
+            Console.WriteLine("28.I like CS2");
+            Console.WriteLine("29.I like DRG");
+            Console.WriteLine("30.I like messing around");
+            Console.WriteLine("31.I like cats");
+            Console.WriteLine("32.I like calicos");
+            Console.WriteLine("33.I like tuxedo cats");
+            Console.WriteLine("34.I like maine coons");
+            Console.WriteLine("35.I like OW2");
+            Console.WriteLine("36.I like terreria");
+            Console.WriteLine("37.I like Half life");
+            Console.WriteLine("38.I like half life 2");
+            Console.WriteLine("39.I like half life alyx");
+            Console.WriteLine("40.I like protal");
+            Console.WriteLine("41.I like portal 2");
+            Console.WriteLine("42.I like left 4 dead");
+            Console.WriteLine("43.I like left 4 dead 2");
+            Console.WriteLine("44.I like CSGO");
+            Console.WriteLine("45.I like splintercell");
+            Console.WriteLine("46.I like minecraft");
+            Console.WriteLine("47.I like unico");
+            Console.WriteLine("48.I like flopa");
+            Console.WriteLine("49.I like mevis");
+            Console.WriteLine("50.I like tacos");
+            Console.WriteLine("51.I like salsa");
+            Console.WriteLine("52.I like nachos");
+            Console.WriteLine("53.I like burritos");
+            Console.WriteLine("54.I like skyrim");
+            Console.WriteLine("55.I like oblivion");
+            Console.WriteLine("56.I like fallout");
+            Console.WriteLine("57.I like fallout 2" );
+            Console.WriteLine("58.I like fallout 3");
+            Console.WriteLine("59.I like fallout new vegas");
+            Console.WriteLine("60.I like fallout 4");
+            Console.WriteLine("61.I like fallout 76");
+            Console.WriteLine("62.I like metal gear");
+            Console.WriteLine("63.I like mario bros");
+            Console.WriteLine("64.I like mario cart");
+            Console.WriteLine("65.I like crysis");
+            Console.WriteLine("66.I like crysis 2");
+            Console.WriteLine("67.I like crysis 3" );
+            Console.WriteLine("68.I like COD BO1" );
+            Console.WriteLine("69.I like COD BO2" );
+            Console.WriteLine("70.I like COD BO3");
+            Console.WriteLine("71.I like COD BO4");
+            Console.WriteLine("72.I like COD BO CW");
+            Console.WriteLine("73.I like die rise");
+            Console.WriteLine("74.I like five");
+            Console.WriteLine("75.I like derisendrac");
+            Console.WriteLine("76.I like tales from the borderlands");
+            Console.WriteLine("77.I like new tales from the borderlands" );
+            Console.WriteLine("78.I like salvador");
+            Console.WriteLine("79.I like Zer0" );
+            Console.WriteLine("80.I like Maya" );
+            Console.WriteLine("81.I like Axton");
+            Console.WriteLine("82.I like Krieg");
+            Console.WriteLine("83.I like Gaige");
+            Console.WriteLine("84.I like lilith");
+            Console.WriteLine("85.I like roland");
+            Console.WriteLine("86.I like brick");
+            Console.WriteLine("87.I like marcus" );
+            Console.WriteLine("88.I like claptrap" );
+            Console.WriteLine("89.I like mortikai" );
+            Console.WriteLine("90.I like blood wing" );
+            Console.WriteLine("91.I like talon");
+            Console.WriteLine("92.I like tiny tina");
+            Console.WriteLine("93.I like zane");
+            Console.WriteLine("94.I like corn");
+            Console.WriteLine("95.I like captian flint");
+            Console.WriteLine("96.I like hansome jack");
+            Console.WriteLine("97.I like commander nox" );
+            Console.WriteLine("98.I like tyreen colipso" );
+            Console.WriteLine("99.I like troy colipso");
+            Console.WriteLine("100.I like Docter ZED");
+        }
+    }
+}
